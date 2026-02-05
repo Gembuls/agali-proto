02c43c7f1a3dd5cf20543d8c5d7b8e2de546f775
Agali Cogati <agalicogati@gmail.com>
2026-02-05 16:28:30 +0700

    Penambahan Beberapa Fitur

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,25 @@ function App() {
   return (
     <>
       <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
-        <div>
-          <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
-            <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md"/>
+        <div className="animate__animated animate__fadeInUp animate__delay-3s">
+          <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl ">
+            <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md" loading="lazy"/>
             <q>Investasi Terbaik Adalah Investasi Pada Diri Sendiri</q>
           </div>
           <h1 className="text-5xl/tight font-bold mb-6">Hi, nama saya Agali.</h1>
           <p className="text-base/loose mb-6 opacity-50">Saya mempunyain ketertarikan dalam bidang Teknologi terutama Pemrograman dan Design, Ketertarikan dalam bidang ini sudah berlangsung lebih dari 4 Tahun Terakhir.</p>
           <div className="flex items-center sm:gap-4 gap-2">
-            <a href="#" className="bg-blue-700 p-4 rounded-2xl hover:bg-blue-600">Download CV <i className="ri-download-line ri-lg"></i></a>
-            <a href="#" className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600">Lihat Proyek <i class="ri-arrow-down-fill ri-lg"></i></a>
+            <a href="#" className="bg-blue-700 p-4 rounded-2xl hover:bg-blue-600">Download CV<i className="ri-download-line ri-lg"></i></a>
+            <a href="#" className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600">Lihat Proyek<i className="ri-arrow-down-fill ri-lg"></i></a>
           </div>
         </div>
-        <img src={DataImage.HeroImage} alt="Hero Image" className="w-[400px] md:ml-auto rounded-2xl"/>
+        <img src={DataImage.HeroImage} alt="Hero Image" className="w-[400px] md:ml-auto rounded-2xl animate__animated animate__backInDown animate__delay-4s" loading="lazy"/>
       </div>
 
       {/* TENTANG */}
-      <div className="about mt-32 py-10">
-        <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-600 rounded-lg">
-          <img src={DataImage.HeroImage} alt="image" className="w-12 rounded-md mb-10 sm:hidden" />
+      <div className="about mt-32 py-10" id="tentang">
+        <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-600 rounded-lg" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">
+          <img src={DataImage.HeroImage} alt="image" className="w-12 rounded-md mb-10 sm:hidden" loading="lazy"/>
           <p className="text-base/loose mb-10">
             Halo, Perkenalkan saya Yudhas Agali Arya Cogati kerap di panggil Agali, 
             Saya seorang mahasiswa di Perguruan tinggi Politeknik Negeri Lampung Program Studi Rekayasa Perangkat Lunak, 
@@ -33,7 +33,7 @@ function App() {
             sehingga proyek yang saya kembangkan tidak hanya menarik tetapi juga memberikan pengalaman pengguna yang optimal.
           </p>
           <div className="flex items-center justify-between">
-            <img src={DataImage.HeroImage} alt="image" className="w-12 rounded-md sm:block hidden"/>
+            <img src={DataImage.HeroImage} alt="image" className="w-12 rounded-md sm:block hidden" loading="lazy"/>
             <div className="flex items-center gap-6">
               <div>
                 <h1 className="text-4xl mb-1">0 <span className="text-blue-500">+</span>
@@ -52,11 +52,11 @@ function App() {
         <div className="tools mt-32">
           <h1 className="text-4xl/snug font-bold mb-4">Tools Yang Di Pakai</h1>
           <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50">Berikut Ini Adalah Tols Yang Saya Pakai</p>
-          <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+          <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4" data-aos="fade-up" data-aos-once="true">
             {listTools.map((tool) => (
               <div className="flex items-center gap-2 p-3 border border-zinc-600 
-              rounded-md hover:bg-zinc-800 group" key={tool.id}>
-              <img src={tool.gambar} alt="Image tols" className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900"/>
+              rounded-md hover:bg-zinc-800 group" data-aos="fade-up" data-aos-duration="1000" data-aos-delay={tool.dad} data-aos-once="true" key={tool.id}>
+              <img src={tool.gambar} alt="Image tols" className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900" loading="lazy"/>
               <div>
                 <h4 className="font-bold">{tool.nama}</h4>
                 <p className="opacity-50">{tool.ket}</p>
@@ -69,13 +69,13 @@ function App() {
       {/* TENTANG */}
 
       {/* PROYEK */}
-      <div className="proyek mt-32 py-10">
-        <h1 className="text-center text-4xl font-bold mb-2">Proyek</h1>
-        <p className="text-base/loose text-center opacity-50">Berikut Ini Adalah Proyek Yang Telah Saya Buat</p>
+      <div className="proyek mt-32 py-10" id="proyek">
+        <h1 className="text-center text-4xl font-bold mb-2" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">Proyek</h1>
+        <p className="text-base/loose text-center opacity-50" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300" data-aos-once="true">Berikut Ini Adalah Proyek Yang Telah Saya Buat</p>
         <div className="proyek-box mt-14 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
             {listProyek.map((proyek) => (
-              <div key={proyek.id} className="p-4 bg-zinc-800 rounded-lg">
-                <img src={proyek.gambar} alt="Proyek gambar" />
+              <div key={proyek.id} className="p-4 bg-zinc-800 rounded-lg" data-aos="fade-up" data-aos-duration="1000" data-aos-delay={proyek.dad} data-aos-once="true">
+                <img src={proyek.gambar} alt="Proyek gambar" loading="lazy" />
                 <div>
                   <h1 className="text-2xl font-bold my-4">{proyek.nama}</h1>
                   <p className="text-base/loose mb-4 opacity-50">{proyek.desk}</p>
@@ -93,6 +93,32 @@ function App() {
         </div>
       </div>
       {/* PROYEK */}
+
+      {/* KONTAK */}
+      <div className="kontak mt-32 sm:p-10 p-0" id="kontak">
+        <h1 className="text-4xl mb-2 font-bold text-center">kontak</h1>
+        <p className="text-base/loose text-center mb-10 opacity-50">Mari Terhubung Dengan Saya</p>
+        <form action="https://formsubmit.co/yudhascogati@gmail.com" method="POST" className="bg-zinc-800 p-10 sm:w-fit w-full mx-auto rounded-md" autoComplete="off">
+          <div className="flex flex-col gap-6 ">
+            <div className="flex flex-col gap-2">
+              <label className="font-semibold">Nama Lengkap</label>
+              <input type="text" name="nama" placeholder="Masukan Nama.." className="border border-zinc-500 p-2 rounded-md"required />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label className="font-semibold">Email</label>
+              <input type="text" name="Email" placeholder="Masukan email.." className="border border-zinc-500 p-2 rounded-md"required />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="Pesan" className="font-semibold">Pesan</label>
+              <textarea name="Pesan" id="Pesan" cols="45" rows="7" placeholder="Masukan Pesan..." className="border border-zinc-500 p-2 rounded-md" required></textarea>
+            </div>
+            <div className="text-center">
+              <button type="submit" className="bg-blue-700 p-3 rounded-lg w-full border border-zinc-600 hover:bg-blue-600">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+      {/* KONTAK */}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,24 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import Navbar from './componentes/Navbar.jsx';
+import 'animate.css';
 
 import 'remixicon/fonts/remixicon.css';
+import Footer from './componentes/footer.jsx';
+import PreLoad from './componentes/PreLoad.jsx';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <PreLoad />
     <div className="container mx-auto px-4">
       <Navbar />
       <App />
+      <Footer />
     </div>
   </StrictMode>,
 );
